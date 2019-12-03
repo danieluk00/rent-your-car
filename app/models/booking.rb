e9e4bbb4d4b555d824e3c9fa8dd1e53ac30dd076
elena perez <e_j_perez@hotmail.com>
2019-12-03 12:57:51 +0100

    Validations have been added

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,9 @@
 class Booking < ApplicationRecord
+  has_many :cars, dependent: :destroy
+  has_many :reviews, presence: true
+  validates :start_date, presence: true, availability: true
+  validates :end_date, presence: true, availability: true
+  validates :total_price, presence: true
+  validates :status, presence: true
+
 end
